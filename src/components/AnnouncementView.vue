bc6db2dbf91f6f97d361a626fe72a5167f455635
lirongyue <lirongyue030215@163.com>
2026-09-02 19:01:39 +0800

    Build ICUI 2027 conference announcement page

--- src/components/AnnouncementView.vue
+++ src/components/AnnouncementView.vue
@@ -1,4 +1,21 @@
 <script setup>
+import anthonyYeh from '../../anthony yeh.png'
+import christianJensen from '../../cHRISTIAM S.Jensen.png'
+import wenzhongShi from '../../John Shi.png'
+import liqiuMeng from '../../Li Qiu Meng.png'
+import qingquanLi from '../../LiQINGQUAN.png'
+import michaelBatty from '../../michael batty.png'
+import michaelGoodchild from '../../Michael_GOODCHILD.png'
+import pengCui from '../../pengcui.png'
+import renzhongGuo from '../../renzhongguo.png'
+import zhiqiangWu from '../../wuzhiqiang.png'
+import carloRatti from '../../Carlo Ratti.png'
+import jianyaGong from '../../Gongjianya.png'
+import mayYuan from '../../May Yuan.png'
+import meiPoKwan from '../../meipoKuan.png'
+import shihLungShaw from '../../Shih-Lung Shaw.png'
+import yingJin from '../../yingjin.png'
+
 const awards = [
   'Outstanding Achievement in Urban Informatics Award',
   'Urban Informatics Journal Paper of the Year Award',
@@ -16,6 +33,122 @@ const activities = [
   'Networking session with Editors-in-Chief of International Journals',
   'Oral Presentation',
 ]
+
+const keynoteSpeakers = [
+  {
+    name: 'Michael Batty',
+    recognition: 'Fellow of the British Academy and the Royal Society',
+    role: 'Chair, Centre for Advanced Spatial Analysis; Emeritus Professor of Planning',
+    institution: 'University College London',
+    image: michaelBatty,
+  },
+  {
+    name: 'Peng Cui',
+    recognition: 'Academician of the Chinese Academy of Sciences',
+    role: 'Scientist',
+    institution: 'Institute of Geographic Sciences and Natural Resources Research, Chinese Academy of Sciences',
+    image: pengCui,
+  },
+  {
+    name: 'Jianya Gong',
+    recognition: 'Academician of the Chinese Academy of Sciences',
+    role: 'Professor of Remote Sensing; Former Dean',
+    institution: 'School of Remote Sensing and Information Engineering, Wuhan University',
+    image: jianyaGong,
+  },
+  {
+    name: 'Michael Goodchild',
+    recognition: 'Member of the US National Academy of Sciences',
+    role: 'Professor Emeritus of Geography',
+    institution: 'University of California, Santa Barbara',
+    image: michaelGoodchild,
+  },
+  {
+    name: 'Renzhong Guo',
+    recognition: 'Academician of the Chinese Academy of Engineering',
+    role: 'Dean of Research Institute for Smart Cities & Professor',
+    institution: 'Shenzhen University',
+    image: renzhongGuo,
+  },
+  {
+    name: 'Christian S. Jensen',
+    recognition: 'Member of the Academia Europaea',
+    role: 'Professor of Computer Science',
+    institution: 'Aalborg University',
+    image: christianJensen,
+  },
+  {
+    name: 'Ying Jin',
+    recognition: 'Director, Martin Centre for Architectural and Urban Studies',
+    role: 'Professor of Architecture and Urbanism',
+    institution: 'Department of Architecture, University of Cambridge',
+    image: yingJin,
+  },
+  {
+    name: 'Mei-Po Kwan',
+    recognition: 'Professor of Geography and Geographic Information Science',
+    role: 'Urban GIS and Spatial Analysis Scholar',
+    institution: 'University of Illinois at Urbana-Champaign',
+    image: meiPoKwan,
+  },
+  {
+    name: 'Qingquan Li',
+    recognition: 'Academician of the Chinese Academy of Engineering',
+    role: 'Party Secretary & Professor',
+    institution: 'Shenzhen University',
+    image: qingquanLi,
+  },
+  {
+    name: 'Liqiu Meng',
+    recognition: 'Member of the German National Academy of Sciences',
+    role: 'Professor',
+    institution: 'Technical University of Munich',
+    image: liqiuMeng,
+  },
+  {
+    name: 'Carlo Ratti',
+    recognition: 'Director, MIT Senseable City Lab',
+    role: 'Professor of the Practice of Urban Technologies',
+    institution: 'Massachusetts Institute of Technology',
+    image: carloRatti,
+  },
+  {
+    name: 'Shih-Lung Shaw',
+    recognition: 'Alvin and Sally Beaman Professor',
+    role: 'Professor of Geography',
+    institution: 'University of Tennessee, Knoxville',
+    image: shihLungShaw,
+  },
+  {
+    name: 'Zhiqiang Wu',
+    recognition: 'Academician of the Chinese Academy of Engineering',
+    role: 'Professor',
+    institution: 'Tongji University',
+    image: zhiqiangWu,
+  },
+  {
+    name: 'Anthony Yeh',
+    recognition: 'Academician of the Chinese Academy of Sciences',
+    role: 'Chair Professor',
+    institution: 'The University of Hong Kong',
+    image: anthonyYeh,
+  },
+  {
+    name: 'May Yuan',
+    recognition: 'Ashbel Smith Professor of Geospatial Information Sciences',
+    role: 'GIS Ph.D. Director',
+    institution: 'School of Economic, Political, and Policy Sciences, University of Texas at Dallas',
+    image: mayYuan,
+  },
+]
+
+const conferenceChair = {
+  name: 'Wenzhong Shi',
+  recognition: 'Academician of the European Academy of Sciences',
+  role: 'President of International Society for Urban Informatics; Director of Smart Cities Research Institute',
+  institution: 'The Hong Kong Polytechnic University',
+  image: wenzhongShi,
+}
 </script>
 
 <template>
@@ -37,7 +170,7 @@ const activities = [
 
     <section class="welcome-stage" aria-label="GSCS and ICUI 2027 welcome">
       <div class="welcome-image-frame">
-        <img src="/英文版-香港调整-规整建筑.png" alt="Global Smart Cities Summit cum the International Conference on Urban Informatics 2027" />
+        <img src="/英文版-香港调整-规整建筑-香港中环间距版.png" alt="Global Smart Cities Summit cum the International Conference on Urban Informatics 2027" />
       </div>
     </section>
 
@@ -67,6 +200,38 @@ const activities = [
         </div>
 
       </article>
+    </section>
+
+    <section class="past-speakers" aria-labelledby="past-speakers-title">
+      <div class="past-speakers-content">
+        <div class="section-heading">
+          <h2 id="past-speakers-title">ICUI Keynote Speaker Gallery</h2>
+          <p>* In alphabetical order of surnames</p>
+        </div>
+
+        <div class="speaker-grid">
+          <article v-for="speaker in keynoteSpeakers" :key="speaker.name" class="speaker-card">
+            <img :src="speaker.image" :alt="speaker.name" />
+            <div>
+              <h3>{{ speaker.name }}</h3>
+              <p class="speaker-recognition">{{ speaker.recognition }}</p>
+              <p>{{ speaker.role }}</p>
+              <p>{{ speaker.institution }}</p>
+            </div>
+          </article>
+        </div>
+
+        <article class="chair-card">
+          <img :src="conferenceChair.image" :alt="conferenceChair.name" />
+          <div>
+            <p class="chair-label">Conference Chair</p>
+            <h3>{{ conferenceChair.name }}</h3>
+            <p class="speaker-recognition">{{ conferenceChair.recognition }}</p>
+            <p>{{ conferenceChair.role }}</p>
+            <p>{{ conferenceChair.institution }}</p>
+          </div>
+        </article>
+      </div>
     </section>
 
     <footer class="contact">
